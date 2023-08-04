1d0d453e5367e584a3f55d856a8b12dd089409e4
muzaale <muzaale@gmail.com>
2023-08-04 16:18:52 -0400

    dramatis_personae links, _toy.yml key to success

--- alphabeta.sh
+++ alphabeta.sh
@@ -3,7 +3,7 @@ jb build alpha
 cp -r alpha/* beta
 cd beta
 git add ./*
-git commit -m "dramatis personae links"
+git commit -m "dramatis_personae links, _toy.yml key to success"
 chmod 600 ~/.ssh/id_alphabeta
 ssh-add -D
 git remote set-url origin git@github.com:jhutrc/beta
