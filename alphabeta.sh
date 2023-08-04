0d8c3464cbc11cb40d7219d821df329763936799
muzaale <muzaale@gmail.com>
2023-08-04 15:24:58 -0400

    basic content, next hyperlinks

--- alphabeta.sh
+++ alphabeta.sh
@@ -1,10 +1,13 @@
 cd ~/dropbox/1f.ἡἔρις,κ/1.ontology
 jb build alpha
-cp -r three40/* six100
-cd six100
+cp -r alpha/* beta
+cd beta
 git add ./*
-git commit -m "first jb created manually"
+git commit -m "basic content, next hyperlinks"
+chmod 600 ~/.ssh/id_alphabeta
+ssh-add -D
+git remote set-url origin git@github.com:jhutrc/beta
+ssh-add ~/.ssh/id_alphabeta
+alpha
 git push
 ghp-import -n -p -f _build/html
-chmod +x three40/three40.six100.sh
-./three40/three40.six100.sh
