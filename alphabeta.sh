60133e6751e6b6384c4ed4299c41d8286a9f7cb1
muzaale <muzaale@gmail.com>
2023-08-04 17:11:58 -0400

    evolution of COVID-19 variants

--- alphabeta.sh
+++ alphabeta.sh
@@ -3,7 +3,7 @@ jb build alpha
 cp -r alpha/* beta
 cd beta
 git add ./*
-git commit -m "dramatis_personae links, _toy.yml key to success!!!"
+git commit -m "evolution of COVID-19 variants"
 chmod 600 ~/.ssh/id_alphabeta
 ssh-add -D
 git remote set-url origin git@github.com:jhutrc/beta
