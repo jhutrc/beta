c456f2143bdd22cda2c215bf53131ae38129c680
muzaale <muzaale@gmail.com>
2023-08-06 00:52:31 -0400

    removing titles from supporting pages

--- alphabeta.sh
+++ alphabeta.sh
@@ -3,7 +3,7 @@ jb build alpha
 cp -r alpha/* beta
 cd beta
 git add ./*
-git commit -m "fixed link for faculty~hospitalization of donors" 
+git commit -m "removing titles from supporting pages" 
 chmod 600 ~/.ssh/id_alphabeta
 ssh-add -D
 git remote set-url origin git@github.com:jhutrc/beta
