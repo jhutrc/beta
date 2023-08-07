1e02d3bb06bc41e4e636c9d1ca6a56d624f587e1
muzaale <muzaale@gmail.com>
2023-08-07 01:53:44 -0400

    substantive update to act2scene1: python. introducing google's colab. plus the color-version of our logo

--- alphabeta.sh
+++ alphabeta.sh
@@ -5,7 +5,7 @@ git clone https://github.com/jhutrc/beta
 cp -r alpha/* beta
 cd beta
 git add ./*
-git commit -m "idiomatic translation for we are in this together - yaffe ffena" 
+git commit -m "substantive update to act2scene1: python. introducing google's colab. plus the color-version of our logo" 
 chmod 600 ~/.ssh/id_alphabeta
 ssh-add -D
 git remote set-url origin git@github.com:jhutrc/beta
