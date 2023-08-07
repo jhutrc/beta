d028e794733da3dabd208026ddefe5c0de069c07
muzaale <muzaale@gmail.com>
2023-08-07 02:30:59 -0400

    substantive update to epilogue!!!!!!!!!!!!!

--- alphabeta.sh
+++ alphabeta.sh
@@ -5,7 +5,7 @@ git clone https://github.com/jhutrc/beta
 cp -r alpha/* beta
 cd beta
 git add ./*
-git commit -m "substantive update to act2scene1: python. introducing google's colab. plus the color-version of our logo" 
+git commit -m "substantive update to epilogue!!!!!!!!!!!!!" 
 chmod 600 ~/.ssh/id_alphabeta
 ssh-add -D
 git remote set-url origin git@github.com:jhutrc/beta
