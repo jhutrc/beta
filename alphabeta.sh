b083e7e982c320bfcd07daac2df15e3e8aa4727a
muzaale <muzaale@gmail.com>
2023-08-08 11:27:50 -0400

    victory!!! https://github.com/jhutrc/fena/commits/merge_seasons.docx/seasons.docx

--- alphabeta.sh
+++ alphabeta.sh
@@ -5,7 +5,7 @@ git clone https://github.com/jhutrc/beta
 cp -r alpha/* beta
 cd beta
 git add ./*
-git commit -m "co-pilot suggested tip... use image url from github, not local filepth!" 
+git commit -m "victory!!! https://github.com/jhutrc/fena/commits/merge_seasons.docx/seasons.docx " 
 chmod 600 ~/.ssh/id_alphabeta
 ssh-add -D
 git remote set-url origin git@github.com:jhutrc/beta
